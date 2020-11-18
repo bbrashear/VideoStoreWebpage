7485ecc804bc23806f4893d184c21359c0038315
Blake Brashear <bbrashear94@gmail.com>
2020-11-18 14:16:02 -0500

    Added GetMovies and Index methods to MoviesController, added Index and Details view pages to Customers view, and added index view page to Movies view

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,9 +11,25 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Name = "Shrek!"},
+                new Movie{Id = 2, Name = "Wall-e"}
+            };
+        }
+        //GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movie = new Movie { Name = "Inception" };
             var customers = new List<Customer>
             {
                 new Customer{Name = "Customer 1"},
@@ -27,7 +43,5 @@ namespace Vidly.Controllers
             };
             return View(viewModel);
         }
-
-
     }
 }
