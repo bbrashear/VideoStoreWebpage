921407ac12415973e41180f2352833d114431b1f
Blake Brashear <bbrashear94@gmail.com>
2020-11-19 15:02:35 -0500

    Implemented validations on Customer MVC

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -14,5 +14,11 @@ namespace Vidly.Models
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+
+
+        //these properties are used for the Min18YearsIfAMember custom validation 
+        //and are readonly so they do not accidentally get changed elsewhere
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
     }
 }
