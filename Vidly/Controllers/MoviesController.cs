0249ea3e58e06ac985e9c2a570a41b5e81b89da1
Blake Brashear <bbrashear94@gmail.com>
2020-11-19 10:26:20 -0500

    Populated Genres in the database and edited MoviesController to pull data from database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,26 +5,38 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movies
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie{Id = 1, Name = "Shrek!"},
-                new Movie{Id = 2, Name = "Wall-e"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                HttpNotFound();
+
+            return View(movie);
         }
         //GET: Movies/Random
         public ActionResult Random()
