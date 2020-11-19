99c06e9775d03dbfd458768d913f91c5279495c0
Blake Brashear <bbrashear94@gmail.com>
2020-11-19 10:14:12 -0500

    Added Movies to database, added properties to Movie class, created Genre class

---
+++ Vidly/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
