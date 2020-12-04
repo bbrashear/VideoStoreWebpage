d0b6a49bc4538b8e5531a7b310a6ecf6260a95e2
Blake Brashear <bbrashear94@gmail.com>
2020-12-04 11:35:50 -0500

    Built API for renting movies

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -33,6 +33,8 @@ namespace Vidly.Models
         public DbSet<MembershipType> MembershipTypes { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Rental> Rentals { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
